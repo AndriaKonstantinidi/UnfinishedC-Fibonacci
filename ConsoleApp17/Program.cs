ae3bafb986e9176fb4823d39d84f15902d92a592
AndriaKonstantinidi <a97933199@gmail.com>
2023-05-20 17:39:54 +0400

    Add project files.

---
+++ ConsoleApp17/Program.cs
@@ -0,0 +1,21 @@
+﻿class Program
+{
+    static void Main()
+    {
+        foreach(var item in Evend(21))
+        {
+            Console.WriteLine(item);
+        }
+    }
+    static IEnumerable<int> Evend(int a)
+    {
+        for(int i = 0; i <= a; i++)
+        {
+            int c = i + 1;
+            if(i + i == c)
+            {
+                yield return c;
+            }
+        }
+    }
+}
